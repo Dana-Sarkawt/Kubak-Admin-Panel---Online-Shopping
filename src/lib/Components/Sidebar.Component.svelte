5aed8d1c011e501a8ab83aecef4b2d9eb67fb01e
Dana-Sarkawt <91015045+Dana-Sarkawt@users.noreply.github.com>
2023-11-16 11:13:15 +0300

    Update Category & Icons

--- src/lib/Components/Sidebar.Component.svelte
+++ src/lib/Components/Sidebar.Component.svelte
@@ -5,7 +5,7 @@
 
   import { page } from '$app/stores';
   import { Sidebar, SidebarGroup, SidebarItem, SidebarWrapper, SidebarBrand } from 'flowbite-svelte';
-  import { ChartPieSolid, GridSolid, MailBoxSolid, UserSolid, ArrowRightToBracketSolid, FileEditSolid } from 'flowbite-svelte-icons';
+  import { ChartPieSolid, GridSolid, MailBoxSolid, UserSolid, ArrowRightToBracketSolid, FileEditSolid, LayersSolid, ImageSolid, AddressCardSolid, NewspaperSolid, DesktopPcSolid, TableSolid, TableColumnSolid, TabletSolid, WindowSolid } from 'flowbite-svelte-icons';
 
   $: activeUrl = $page.url.pathname;
   let activeClass = 'flex items-center p-2 text-base font-normal text-white bg-[#CB5500] rounded-l-full';
@@ -19,26 +19,54 @@
 
   <Sidebar {activeUrl} {activeClass} {nonActiveClass} asideClass="w-full ">
     <SidebarWrapper class="bg-[#f17f18] p-0 h-full">
-      <SidebarGroup class="h-full" ulClass="h-full">
+      <SidebarGroup class="h-full " ulClass="h-full pl-3">
         <div class="w-full h-auto flex justify-center items-center pb-5 mt-5">
           <img src="/images/kubak.jpg" alt="" width="50px" height="50px">
         </div>
-     
+    
+        <SidebarItem label="Dashboard" href="/" spanClass="md:text-lg lg:text-xl 2xl:text-4xl ml-2">
+          <svelte:fragment slot="icon">
+            <ChartPieSolid class="w-5 h-5 text-white transition duration-75" />
+          </svelte:fragment>
+        </SidebarItem>
   
-        <SidebarItem label="Dashboard" href="/"></SidebarItem>
+        <SidebarItem label="Categories" href="/category" spanClass="-lg lg:text-xl 2xl:text-4xl ml-2">
+          <svelte:fragment slot="icon">
+            <GridSolid class="w-5 h-5 text-white transition duration-75" />
+          </svelte:fragment>
+        </SidebarItem>
   
-        <SidebarItem label="Categories" href="/category"></SidebarItem>
+        <SidebarItem label="Items" href="/docs/components/alert" spanClass="-lg lg:text-xl 2xl:text-4xl ml-2">
+          <svelte:fragment slot="icon">
+            <LayersSolid class="w-5 h-5 text-white transition duration-75" />
+          </svelte:fragment>
+        </SidebarItem>
   
-        <SidebarItem label="Items" href="/docs/components/alert"></SidebarItem>
+        <SidebarItem label="Cards" href="/docs/components/sidebar"  spanClass="-lg lg:text-xl 2xl:text-4xl ml-2">
+          <svelte:fragment slot="icon">
+            <ImageSolid class="w-5 h-5 text-white transition duration-75" />
+          </svelte:fragment>
+        </SidebarItem>
   
-        <SidebarItem label="Cards" href="/docs/components/sidebar"></SidebarItem>
+        <SidebarItem label="Users" href="/docs/components/badge" spanClass="-lg lg:text-xl 2xl:text-4xl ml-2">
+          <svelte:fragment slot="icon">
+            <AddressCardSolid class="w-5 h-5 text-white transition duration-75" />
+          </svelte:fragment>
+        </SidebarItem>
   
-        <SidebarItem label="Users" href="/docs/components/badge"></SidebarItem>
+        <SidebarItem label="Reports" href="#" spanClass="-lg lg:text-xl 2xl:text-4xl ml-2">
+          <svelte:fragment slot="icon">
+            <NewspaperSolid class="w-5 h-5 text-white transition duration-75" />
+          </svelte:fragment>
+
+        </SidebarItem>
   
-        <SidebarItem label="Reports" href="#"></SidebarItem>
-  
-        <SidebarItem label="Monitoring" href="#"></SidebarItem>
-  
+        <SidebarItem label="Monitoring" href="#" spanClass="-lg lg:text-xl 2xl:text-4xl ml-2">
+
+          <svelte:fragment slot="icon">
+            <DesktopPcSolid class="w-5 h-5 text-white transition duration-75" />
+          </svelte:fragment>
+        </SidebarItem>
   
       </SidebarGroup>
     </SidebarWrapper>
--- src/routes/category/+page.svelte
+++ src/routes/category/+page.svelte
@@ -15,7 +15,7 @@
     { name: "5", href: "/category/5", active:false },
   ];
 
-  let ArrayNumber: number[] = [1, 2, 3, 4, 5, 6, 7, 8];
+  let ArrayNumber: number[] = [1, 2, 3, 4, 5, 6, 7];
   $: {
     pages.forEach((page) => {
       let splitUrl = page.href!.split("?");
@@ -39,22 +39,22 @@
   };
 </script>
 
-<div class="w-full h-auto flex justify-center items-center mt-4">
-  <!-- START ADD CATEGORIES  -->
-  <div
-    class="md:w-16 md:h-16 lg:w-24 lg:h-24 hover:scale-105 transition-ease-in-out duration-300 bg-[#f17f18] px-5 flex justify-center items-center flex-col rounded-xl mb-3"
-  >
-    <p class="md:text-4xl lg:text-6xl text-white">+</p>
-  </div>
-  <!-- END ADD CATEGORIES  -->
-</div>
 
 <div
-  class="w-full h-auto flex flex-row-reverse flex-wrap justify-center items-center md:px-12 lg:px-26 xl:px-26 2xl:px-32 gap-3"
+class="w-full h-auto flex flex-row-reverse flex-wrap justify-center my-12 items-center md:px-12 lg:px-26 xl:px-26 2xl:px-32 gap-3"
 >
+
+
+
+<div
+class=" md:w-28 md:h-36  lg:w-44 lg:h-60 bg-[#f17f18] px-5 flex justify-around items-center flex-col rounded-xl"
+>
+<p class="w-full flex justify-center items-center text-6xl text-white">+</p>
+
+</div>
   {#each ArrayNumber as array}
     <div
-      class="md:w-28 lg:w-36 md:h-36 lg:h-52 xl:w-60 xl:h-64 2xl:w-80 2xl:h-96 bg-white px-5 flex justify-around items-center flex-col rounded-xl"
+      class="md:w-28 md:h-36  lg:w-44 lg:h-60  bg-white px-5 flex justify-around items-center flex-col rounded-xl"
     >
       <img src="/images/rice.png" alt="" />
       <p class="text-2xl">Rice</p>
