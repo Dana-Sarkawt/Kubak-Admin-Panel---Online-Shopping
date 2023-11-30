9dc4df785d2997908286cd1e4171325af69a92f6
RovarQRoom <rovar.07000120@outlook.com>
2023-11-30 14:38:29 +0300

    Update order items in layout component

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -23,21 +23,21 @@
       goto("/");
     }
 
-    // const order: CreateOrderRequest = {
-    //   userId: $authStore!.id,
-    //   items: [
-    //     {
-    //       itemId: "6565e81e63a13ad3a10b",
-    //       quantity: 5,
-    //     },
-    //     {
-    //       itemId: "65671b57f2759839a34d",
-    //       quantity: 3,
-    //     },
-    //   ],
-    //   addressId: "65672699cb03105ab2a7",
-    // };
-    // await ordersRepository.createOrder(order);
+    const order: CreateOrderRequest = {
+      userId: $authStore!.id,
+      items: [
+        {
+          itemId: "6565e81e63a13ad3a10b",
+          quantity: 5,
+        },
+        {
+          itemId: "656739d6e765a81dff7d",
+          quantity: 3,
+        },
+      ],
+      addressId: "65672699cb03105ab2a7",
+    };
+    await ordersRepository.createOrder(order);
   });
 
   async function checkDarkMode() {
