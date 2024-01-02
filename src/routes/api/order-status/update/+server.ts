552f79776301530a5cf1e65e5b74c335fea900e1
RovarQRoom <rovar.07000120@outlook.com>
2024-01-02 10:18:07 +0300

    Add async-lock library for locking critical sections

--- src/routes/api/order-status/update/+server.ts
+++ src/routes/api/order-status/update/+server.ts
@@ -1,6 +1,9 @@
 import { Environment } from "$lib/Env/Environment";
 import type { RequestHandler } from "@sveltejs/kit";
 import { Client, Databases, ID } from "node-appwrite";
+import AsyncLock from "async-lock";
+
+const lock = new AsyncLock();
 
 const client = new Client()
   .setEndpoint(Environment.appwrite_endpoint)
@@ -10,16 +13,19 @@ const client = new Client()
 const database = new Databases(client);
 
 export const POST = (async ({ locals, params, request }) => {
-    const { id,status }:{id:string,status:string} = await request.json();
+  const { id, status }: { id: string; status: string } = await request.json();
 
+  lock.acquire(id, async function (done) {
     await database.updateDocument(
-        Environment.appwrite_database,
-        Environment.appwrite_collection_order_status,
-        id,
-        {
-            status:status
-        }
+      Environment.appwrite_database,
+      Environment.appwrite_collection_order_status,
+      id,
+      {
+        status: status,
+      }
     );
+    done();
+  });
 
   return new Response(`Order Has Been ${status}`);
 }) satisfies RequestHandler;
